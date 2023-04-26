5ee9459f9bada70f3ce638159d16fec7ae6662e5
sheng-kai-wang <76858274+sheng-kai-wang@users.noreply.github.com>
2023-04-27 05:09:30 +0800

    Update install-fonts.sh

--- scripts/install-fonts.sh
+++ scripts/install-fonts.sh
@@ -4,3 +4,11 @@ wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/Ne
 wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold.ttf &&
 wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Italic.ttf &&
 wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold%20Italic.ttf
+
+sudo mkdir -p /usr/share/fonts/truetype/custom
+sudo mv 'MesloLGS NF Bold Italic.ttf' /usr/share/fonts/truetype/custom/
+sudo mv 'MesloLGS NF Bold.ttf' /usr/share/fonts/truetype/custom/
+sudo mv 'MesloLGS NF Italic.ttf' /usr/share/fonts/truetype/custom/
+sudo mv 'MesloLGS NF Regular.ttf' /usr/share/fonts/truetype/custom/
+
+sudo fc-cache -f -v
