a2fd528b00b29b782eb15a5c351126f776ad5ee6
sheng-kai-wang <76858274+sheng-kai-wang@users.noreply.github.com>
2023-04-27 04:08:30 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+sudo sh ./scripts/install-tools.sh
+sudo sh ./scripts/install-fonts.sh
+sudo sh ./scripts/install-zsh.sh
 sudo sh ./scripts/cp-config.sh
+
+source ~/.zshrc
+source ~/.vimrc
+
+p10k configure
