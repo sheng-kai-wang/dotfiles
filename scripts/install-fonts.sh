6f62bf635c0743965e3e53c275735bcd24b1ca55
sheng-kai-wang <76858274+sheng-kai-wang@users.noreply.github.com>
2023-04-27 03:44:32 +0800

    Create install-fonts.sh

---
+++ scripts/install-fonts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Regular.ttf &&
+wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold.ttf &&
+wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Italic.ttf &&
+wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold%20Italic.ttf
