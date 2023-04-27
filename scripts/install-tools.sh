1133416509dd1d6c261aecc7133e373929479c86
sheng-kai-wang <76858274+sheng-kai-wang@users.noreply.github.com>
2023-04-27 09:54:06 +0800

    Update install-tools.sh

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -6,32 +6,25 @@ sudo apt install wget git curl vim tree neofetch htop -y
 
 # install docker
 sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get purge docker-ce docker-ce-cli containerd.io
 sudo rm -rf /var/lib/docker
 sudo rm -rf /var/lib/containerd
 
-sudo mkdir /var/lib/docker
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release -y
-    
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-
-sudo docker run --rm hello-world
+sudo docker run hello-world
 
 # docker no sudo
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker 
+docker --version
